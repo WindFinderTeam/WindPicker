42b3a3c17cb518f9bdd5536d9e31a268626b2fba
maestrolsj <maestrolsj@gmail.com>
2016-09-10 18:12:34 +0900

    devide LocalList into Surf, Gliding

--- GlidingParser.js
+++ GlidingParser.js
@@ -14,11 +14,13 @@ function getGlidingWeather (responseJSON){
 
     var d = new Date();
     var localeDate  =  d.toLocaleDateString();
-    var year = localeDate.substr(6,4);
+    var year = '20'+localeDate.substr(6,4);
     var month = localeDate.substr(0,2);
-    var day = localeDate.substr(3,2);
+    var date = localeDate.substr(3,2);
+    var day = week[new Date(year+'-'+month+'-'+date).getDay()];
     console.log(" year  >>> " + year);
     console.log(" month  >>> " + month);
+    console.log(" date  >>> " + date);
     console.log(" day  >>> " + day);
 
 }
