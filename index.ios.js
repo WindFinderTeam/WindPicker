557ec8aafe119aac8176fbf0a851743ad631557f
maestro <maestrolsj@gmail.com>
2017-03-05 22:19:21 +0900

    move to mac

--- index.ios.js
+++ index.ios.js
@@ -1,66 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import  React, {Component} from 'react';
 import {
     AppRegistry,
     StyleSheet,
+    ScrollView,
     Text,
-    View
+    Image,
+    TouchableOpacity,
+    View,
+    Linking,
+    BackAndroid,
+    StatusBar,
 } from 'react-native';
 
-import LinearGradient from 'react-native-linear-gradient';
 
-export default class WindPicker extends Component {
+import Modal               from 'react-native-simple-modal';
+import VersionCheck        from 'react-native-version-check';
+//import AndroidFirstView    from './AndroidFirstView';
+import Toast, { DURATION } from 'react-native-easy-toast';
+
+//import { realmInstance } from "./RealmHndler.js";
+
+var nowMode = "";
+var lastMode ;
+
+class  WindPicker extends Component {
+
+
+
+
     render() {
+
         return (
-            <View style={styles.container}>
-              <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
-                <Text style={styles.buttonText}>
-                  Sign in with Facebook
-                </Text>
-              </LinearGradient>
-            </View>
+
+            <View><Text>SSSSS</Text></View>// LoadingView or AndroidFirstView
         );
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-    linearGradient: {
 
-        width:300,
-        height:50,
-        paddingLeft: 15,
-        paddingRight: 15,
-        borderRadius: 5
-    },
-    buttonText: {
-        fontSize: 18,
-        fontFamily: 'Gill Sans',
-        textAlign: 'center',
-        margin: 10,
-        color: '#ffffff',
-        backgroundColor: 'transparent',
-    },
-});
 
 AppRegistry.registerComponent('WindPicker', () => WindPicker);
