c65a78984577b0e50c2409365c15e7b897a1d805
maestro <maestrolsj@gmail.com>
2017-03-11 16:09:53 +0900

    initialized

---
+++ android/app/src/main/java/com/windpicker/MainApplication.java
@@ -0,0 +1,50 @@
+package com.windpicker;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import io.xogus.reactnative.versioncheck.RNVersionCheckPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.react.rnspinkit.RNSpinkitPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import io.realm.react.RealmReactPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNVersionCheckPackage(),
+            new VectorIconsPackage(),
+            new RNSpinkitPackage(),
+            new LinearGradientPackage(),
+            new RealmReactPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
